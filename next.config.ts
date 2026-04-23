009ef74ef361aff13c3a500203d517d1ab032786
maiellanuqui <mnuqui@uci.edu>
2026-04-22 21:46:21 -0700

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/Anchor',
+  assetPrefix: '/Anchor',
 };
 
 export default nextConfig;
